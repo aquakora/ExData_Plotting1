4b8d1ade7ee5db0662434cdbd57d1ab5f0f50c8a
sweadon <sweadon@cpsm.us>
2016-04-10 21:01:36 -0400

    initial plot2 script

---
+++ plot2.R
@@ -0,0 +1,30 @@
+### Script creates Plot1.png for Coursera Exploratory Data Analysis Assignment 1
+library(readr)
+library(dplyr)
+library(lubridate)
+
+## Step 1. Download Data
+download.site <- "https://d396qusza40orc.cloudfront.net/"
+download <- "exdata%2Fdata%2Fhousehold_power_consumption.zip"
+save.file.as <- "household_power_consumption.zip"
+
+download.file(paste0(download.site, download), save.file.as, method = "curl")
+unzip(save.file.as)
+
+# Step 2. Read in Data
+power.readings <- read_csv2("household_power_consumption.txt",
+                            col_types = "ccnnnnnnn", na = "?")
+names(power.readings) <- gsub("_", ".", tolower(names(power.readings)))
+
+start.date <- dmy_hms("01/02/2007 00:00:00")
+end.date <-  dmy_hms("02/02/2007 23:59:59")
+power.readings <- power.readings %>%
+  mutate(date.time = dmy_hms(paste(date, time, sept = " "))) %>%
+  filter(between(date.time, start.date, end.date)) %>%
+  select(10, 3:9)
+
+plot(power.readings$date.time, power.readings$global.active.power / 1000,
+       type = "l", ylab = "Global Active Power (killowatts)", xlab = "")
+
+dev.copy(png, "plot2.png")
+dev.off()
